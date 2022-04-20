ca0f7d9a3235267d4c86f940314da577658284a6
Git_TJ <98149148+GeoffreyGichaga@users.noreply.github.com>
2022-04-20 17:19:41 +0300

    commit tree 6b01a4bf63debc1dea727fe5f0928edb78c8c237

--- index.js
+++ index.js
@@ -1,1 +1,54 @@
 // Your code here
+const dodger = document.getElementById('dodger')
+dodger.style.backgroundColor = "#FF69B4"
+dodger.style.bottom = '0px'
+dodger.style.left = 'px'
+
+
+
+
+function moveDodgerLeft()
+{
+    let doggerLeft = dodger.style.left.replace('px','')
+    let leftDistance = parseInt(doggerLeft,10)
+
+    if(leftDistance > 0)
+    {
+        dodger.style.left = `${leftDistance - 1 }px`
+
+    }
+    
+}
+document.addEventListener('keydown',(e)=>{
+
+    if(e.key === 'ArrowLeft')
+    {
+        moveDodgerLeft();
+    }
+})
+
+
+
+
+
+
+function moveDodgerRight()
+{
+    let doggerLeft = dodger.style.left.replace('px','')
+    let leftDistance = parseInt(doggerLeft,10)
+
+    if(leftDistance  > 0)
+    {
+        dodger.style.left = `${leftDistance + 1 }px`
+
+    }
+ 
+}
+
+document.addEventListener('keydown',(e)=>{
+    if(e.key === 'ArrowRight')
+    {
+        moveDodgerRight()
+    }
+})
+
